2bcc72b185a9be5ad530483c29b4c19a43b0dd1c
Emmanuel Frati <emmanuelfrati@gmail.com>
2017-11-07 09:53:32 -0300

    Modifico el listado de docentes para que la lupa tenga un enlace al perfil y que muestre 50 docentes por página

--- view/docentes.php
+++ view/docentes.php
@@ -25,8 +25,8 @@
                 </thead>
 
                 <tbody>
-                    <tr class='clickable-row' data-href='index.php?c=docente&a=perfil&idd={{profe.idDocente}}' dir-paginate="profe in profesores | filter:busqueda | orderBy:columna:reverse | itemsPerPage:15"> 
-                        <td><img src="view/images/lupa.jpg" style="width:20px; height:20px;"></td>
+                    <tr class='clickable-row' data-href='index.php?c=docente&a=perfil&idd={{profe.idDocente}}' dir-paginate="profe in profesores | filter:busqueda | orderBy:columna:reverse | itemsPerPage:50"> 
+                        <td><a href='index.php?c=docente&a=perfil&idd={{profe.idDocente}}'><img src="view/images/lupa.jpg" style="width:20px; height:20px;"></a></td>
                         <td>{{profe.nombre}}</td>
                         <td>{{profe.apellido}}</td>
                         <td>{{profe.email}}</td>
